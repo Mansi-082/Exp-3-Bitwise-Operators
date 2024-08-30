a95f5f0b975a207929911d779548bd9635b90ea9
Mansi-082 <mansi.kulkarni.btech2023@sitpune.edu.in>
2024-08-30 10:50:15 +0530

    2. Bit Position

--- BitPosition.cpp
+++ BitPosition.cpp
@@ -7,21 +7,26 @@
 using namespace std;
 
 int main() {
-    int num, set, reset;
-    // Input the number and bit positions to set and reset
+    int num, setPos, resetPos;
+
+    // Taking inputs from the user
     cout << "Enter a number: ";
     cin >> num;
     cout << "Enter the bit position to set (0 to 7): ";
-    cin >> set;
+    cin >> setPos;
     cout << "Enter the bit position to reset (0 to 7): ";
-    cin >> reset;
-    // Display the original number in binary
-    cout<<num<<" in binary is "<<bitset<8>(num)<<endl;
-    // Set the bit at set_pos
-    int num_set = num | (1 << set);
-    cout << "Result after setting bit number " <<set<< " is " << bitset<8>(num_set) << endl;
-    // Reset the bit at reset_pos
-    int num_reset = num_set & ~(1 << reset);
-    cout << "Result after resetting bit number " <<reset<< " is " << bitset<8>(num_reset) << endl;
+    cin >> resetPos;
+
+    // Display the input in binary form
+    cout << num<<" in binary is: " << bitset<8>(num) << endl;
+
+    // Set the specified bit
+    num |= (1 << setPos);
+    cout << "Result after setting bit number " << setPos << " is: " << bitset<8>(num) << endl;
+
+    // Reset the specified bit
+    num &= ~(1 << resetPos);
+    cout << "Result after resetting bit number " << resetPos << " is: " << bitset<8>(num) << endl;
+
     return 0;
 }
